e2435c5ddf79616efde978bed4a1b703504992cb
CODSensei <krishna2342005@gmail.com>
2023-12-13 08:43:27 +0530

    failed-pattern

--- test.c
+++ test.c
@@ -1,24 +1,8 @@
 #include<stdio.h>
 int main() {
-    int n,c=0;
-    printf("\n Enter number\t");
-    scanf("%d",&n);
-    if (n==0||n==1)
-{
-    printf("0 and 1 not prime");
-
-}
-for (int i=1;i<=n;i++){
-    if (n%i==0)
-    
+    for(int i=1;i<=5;i++)
     {
-        c++;
+            printf("*");
     }
-
-}
-if (c==2)
-printf("number is prime");
-else
-printf("Number is not prime");
 return 0;
 }
